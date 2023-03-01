639a68a65ab059d8c126bc172b0c22b01f405734
Shraddha <jadhavshradddha5@gmail.com>
2023-03-01 19:06:48 +0530

    Enter a valid first name

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import com.bridgelab.user.UserRegistration;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Welcome to user registration");
+
+        UserRegistration uc = new UserRegistration();
+        uc.checkFirstName();
     }
 }
